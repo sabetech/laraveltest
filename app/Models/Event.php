5f8c9b8fdb47ee4aa08d987f6838887b545c3275
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-12-26 12:44:03 +0000

    Return Events including workshops

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+
+    public function workshops()
+    {
+        return $this->hasMany(Workshop::class);
+    }
+
+    // public function scopeFutureEvents($query)
+    // {
+    //     return $query->where('start', '>', now());
+    // }
+
+    // public function scopeWarmupEvents($query)
+    // {
+    //     return $query->where('start', '<', now());
+    // }
+
 }
